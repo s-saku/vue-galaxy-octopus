d1c166c726174b2e68150d97925e872da3a50e66
Shinya Sakurai <t.nishi7@gmail.com>
2016-11-25 22:43:19 +0900

    change to vue

---
+++ js/components/GallaxyOctopus.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    test!!
+  </div>
+</template>
+
+<script>
+// import Canvas from './Canvas'
+// import Setting from './settings/Setting'
+
+export default {
+
+  data () {
+    return {
+      setting: {
+          reverseGravity: true,
+          pipeInterval: 1600,
+          noHit: false
+      }
+    }
+  },
+
+  computed: {
+
+  },
+
+  components: {
+  },
+
+  created () {
+  },
+
+  methods: {
+    // onChangeSetting(setting) {
+    //     // set game parameter
+    //     this.setState({ setting: setting }, () => {
+    //         this.refs.canvas.reset()
+    //     })
+    // }
+  }
+}
+</script>
--- webpack.config.js
+++ webpack.config.js
@@ -10,17 +10,26 @@ module.exports = {
   output: {
     filename: '[name].js'
   },
-  
+
   devtool: 'inline-source-map',
 
   module: {
     loaders: [
-      { test: /\.js$|\.jsx$/, exclude: /node_modules|build/, loader: 'babel-loader?experimental&optional=runtime'}
+      { test: /\.js$|\.vue$/, exclude: /node_modules|build/, loader: 'vue'}
     ]
+  },
+
+  // vue-loader config:
+  // lint all JavaScript inside *.vue files with ESLint
+  // make sure to adjust your .eslintrc
+  vue: {
+    loaders: {
+      js: 'babel-loader'
+    }
   },
 
   resolve: {
     root: __dirname,
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.vue']
   }
 };
